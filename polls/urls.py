a67e19481a149e102e7ad8b2e78bb9879f2163df
Pramochete <orientedresearch@gmail.com>
2019-08-24 12:20:38 +0545

    used APIView to create api

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
-from .views import polls_detail, polls_list
+from .apiviews import PollList, PollDetail
 
 urlpatterns = [
-    path('polls/', polls_list, name='polls_list'),
-    path('polls/<int:pk>', polls_detail, name='polls_detail')
+    path('polls/', PollList.as_view(), name='polls_list'),
+    path('polls/<int:pk>', PollDetail.as_view(), name='polls_detail')
 ]
